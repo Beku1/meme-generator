4b6e49e1b1ddada11c2676e3d640c8ac64a4b276
Beny.K <Beku1997@gmail.com>
2021-10-27 11:19:34 +0300

    Phase 1 middle part

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,15 @@
 'use strict'
+
+function init(){
+    initMemes()
+    var imgs = getImgs()
+
+}
+
+
+
+function renderGallery(){
+    let imgs = getImgs()
+    let elGallery = document.querySelector('.gallery')
+   
+}
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,2 +1,37 @@
 'use strict'
 
+var gImgs = []
+var gImgId= 19
+var gMeme = []
+
+
+function init(){
+   gImgs = createImgs()
+   
+}
+
+
+function createImg(id,keywords){
+    let meme = {
+        id,
+        url:`/img/${id}.jpg`,
+        keywords
+    }
+    return meme
+}
+
+function createImgs(){
+    var keywords = [['politics','funny'],['cute','love'],['cute','sleepy'],['cute','sleepy'],['cute','success','funny','OG'],['aliens','funny','OG'],['surprised','funny'],['OG','funny'],
+         ['cute','funny'],['OG','funny','politics'],['sports'],['israel','funny'],['OG','epic'],['OG','matrix','epic','funny'],['OG','funny'],['funny'],['politics','funny'],['funny']]
+         let imgs = []
+    for(var i = 1 ; i <= 18 ;i++){
+      imgs.push(createImg(i,keywords[i-1]))
+    }
+    return imgs
+}
+
+
+function getImgs(){
+    return gImgs;
+}
+
